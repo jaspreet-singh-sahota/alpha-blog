ac7ae225fabcccfa2fe442e5c3c2206592d05db0
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-05-11 18:23:12 +0530

    add delete action for users and all the associated articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-   before_action :set_user, only: [:show, :edit, :update]
+   before_action :set_user, only: [:show, :edit, :update :destroy]
    before_action :require_user, only: [:edit, :update]
    before_action :require_same_user, only: [:edit, :update]
 
@@ -38,6 +38,12 @@ class UsersController < ApplicationController
       @articles = @user.articles.paginate(page: params[:page], per_page: 3)  
    end
 
+   def destroy
+      @user.destroy
+      session[:user_id] = nil
+      flash[:notice] = "User account and all the associated articles are deleted"
+      render_to articles_path
+   end
    private
    def set_user
       @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false}, 
                     length: { maximum: 50 },
                     format: { with: VALID_EMAIL_REGEX }
-    has_many :articles
+    has_many :articles , dependent: :destroy
     has_secure_password
 
 end
