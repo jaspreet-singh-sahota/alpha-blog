7deeab8d09b2fa6574c75c02097b4e1a283baaf2
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-05-11 07:27:42 +0530

    add sing up page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,16 @@ class UsersController < ApplicationController
    end 
 
    def create
-      @user = User.new
-      
+      @user = User.new(params.require(:user).permit(:name, :email, :password))
+      if @user.save
+         flash[:notice] = "Welcome to alpha blog #{@user.name}, you have successfully sign up"
+         redirect_to articles_path
+      else
+         render 'new'
+      end
+   end
+
+   def edit
+      @user = User.find(params.require(:user).permit(:name, :email, :password))
    end
 end
