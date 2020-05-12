e3d5cab70f5bda88be39436766ec6c9a1703340d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-05-12 10:28:44 +0530

    add admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
    def destroy
       @user.destroy
-      session[:user_id] = nil
+      session[:user_id] = nil if @user == current_user
       flash[:notice] = "User account and all the associated articles are deleted"
       redirect_to articles_path
    end
@@ -50,8 +50,8 @@ class UsersController < ApplicationController
    end
 
    def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only edit your own account"
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit and delete your own account"
       redirect_to @user
     end
   end
